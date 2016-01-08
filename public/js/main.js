a8a3396bd4694eb6285af91221734c0dc1643aca
Roman Gorel <r.gorel@drooms.com>
2016-01-08 18:39:12 +0100

    switched storage to pouchdb; removed node_modules from version control; implemented results scoring

--- public/js/main.js
+++ public/js/main.js
@@ -5,36 +5,64 @@
   var searchInput = document.getElementById('search-input');
   var resultsList = document.getElementById('results-list');
 
+  disable();
+  updateButton.innerHTML = 'Loading database...';
+
   autocomplete.init({
     url: '/db',
     limit: 10,
-    onUpdateStart: function() {
-      updateButton.innerHTML = 'Updating database...';
-      updateButton.disabled = true;
-    },
-    onUpdate: function() {
-      updateButton.disabled = false;
-      updateButton.innerHTML = 'Update database';
-      getLastUpdated();
-    }
-  });
+    onUpdateStart: disable,
+    onUpdate: enable
+  }).then(enable);
 
   updateButton.onclick = autocomplete.update;
 
   searchInput.onkeyup = search;
 
-  function search() {
-    resultsList.innerHTML = '';
-    results = autocomplete.search(searchInput.value);
+  var searchFunction;
 
-    results.forEach(function(result) {
-      resultsList.innerHTML += '<li>' + result + '</li>';
-    });
+  function search() {
+    if (typeof searchFunction === 'number') clearTimeout(searchFunction);
+
+    searchFunction = setTimeout(function() {
+      resultsList.innerHTML = '';
+      results = autocomplete.search(searchInput.value);
+
+      results.forEach(function(result) {
+        resultsList.innerHTML += '<li>' + result + '</li>';
+      }, 100);
+
+    })
   }
 
   function getLastUpdated() {
-    lastUpdatedLabel.innerHTML = autocomplete.getLastUpdated();
+    autocomplete.getLastUpdated()
+      .then(function(result) {
+        lastUpdatedLabel.innerHTML = result;
+      });
   }
+
+  function disable() {
+    updateButton.innerHTML = 'Updating database...';
+    updateButton.disabled = true;
+    hide(searchInput);
+  }
+
+  function enable() {
+    updateButton.disabled = false;
+    updateButton.innerHTML = 'Update database';
+    show(searchInput);
+    getLastUpdated();
+  }
+
+  function hide(el) {
+    (el.className.indexOf('hidden') === -1) && (el.className += ' hidden');
+  }
+
+  function show(el) {
+    el.className = el.className.replace(/ hidden/g, '');
+  }
+
 
   getLastUpdated();
 
